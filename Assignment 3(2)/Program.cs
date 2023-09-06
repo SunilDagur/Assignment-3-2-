384fbbfbde46152c57a041c60eeed791f6ceb0e6
Sunil Dagur <sunildagur566@gmail.com>
2023-09-06 22:09:49 +0530

    Add project files.

---
+++ Assignment 3(2)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ClassA
+{
+    private int privateFieldA = 88;
+
+    public int GetPrivateFieldA()
+    {
+        return privateFieldA;
+    }
+}
+
+class ClassB
+{
+    public int AccessPrivateFieldFromA()
+    {
+        ClassA classAObject = new ClassA();
+        return classAObject.GetPrivateFieldA();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClassB classBObject = new ClassB();
+
+        int valueFromA = classBObject.AccessPrivateFieldFromA();
+        Console.WriteLine("Value from ClassA: " + valueFromA);
+    }
+}
